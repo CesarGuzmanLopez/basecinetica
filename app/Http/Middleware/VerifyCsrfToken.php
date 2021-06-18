e17baf174377db4a7f86b092a7d4f9eeed86d8dc
Cesar Guzman <47696662+CesarGuzmanLopez@users.noreply.github.com>
2021-06-18 12:22:43 -0500

    asd

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+
+        "http://chemistry.agalano.com/ModifyBD/DB-pk-Koverall/Solvents"
     ];
 }
